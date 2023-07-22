fefb2fc59a864b66d21eb2d50bdb0970144d9b6f
marcuspang <marcuspangyuyang@gmail.com>
2023-07-22 19:04:56 +0200

    feat: tidy up tba page

--- frontend/src/pages/[tbaAddress]/sponsorships.tsx
+++ frontend/src/pages/[tbaAddress]/sponsorships.tsx
@@ -1,27 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useAccountSponsorableTokenUri } from "@/lib/generated";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { isAddress } from "viem";
 import { Address, useNetwork } from "wagmi";
 
-function parseTokenUri(tokenUri: string): Promise<string> {
-  try {
-    if (tokenUri.startsWith("https://")) {
-      return fetch(tokenUri)
-        .then((res) => res.json())
-        .then((metadata) => metadata.image);
-    }
-  } catch (e) {
-    console.log("Error parsing", tokenUri, e);
-  }
-  return Promise.resolve(tokenUri);
-}
-
 export default function SponsorshipPage() {
   const router = useRouter();
   const { chain } = useNetwork();
   const { tbaAddress } = router.query;
   const [finalImageUrl, setFinalImageUrl] = useState("");
+  const [parsedTokenUri, setParsedTokenUri] = useState("");
 
   const { data: tokenUri } = useAccountSponsorableTokenUri({
     account: tbaAddress as Address,
@@ -38,11 +34,23 @@ export default function SponsorshipPage() {
     }
   }, [tokenUri]);
 
-  console.log(finalImageUrl);
+  async function parseTokenUri(tokenUri: string): Promise<string> {
+    try {
+      if (tokenUri.startsWith("https://")) {
+        const res = await fetch(tokenUri);
+        const parsedTokenUri = await res.json();
+        setParsedTokenUri(parsedTokenUri);
+        return parsedTokenUri.image;
+      }
+    } catch (e) {
+      console.log("Error parsing", tokenUri, e);
+    }
+    return tokenUri;
+  }
 
   return (
     <main className="min-h-[calc(100vh-72px)] container mt-12 grid grid-cols-5 gap-2">
-      <div className="col-span-3 px-2">
+      <div className="col-span-3 px-4">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-4">
           Your TBA Sponsorship
         </h1>
@@ -54,9 +62,27 @@ export default function SponsorshipPage() {
           Active Sponsorships
         </h2>
       </div>
-      <div className="col-span-2">
-        {tokenUri && <img src={finalImageUrl} className="w-full" />}
-      </div>
+      {tokenUri ? (
+        <div className="col-span-2 px-4 space-y-4">
+          <img src={finalImageUrl} className="w-full" alt="Your TBA image" />
+          <Card>
+            <CardHeader>
+              <CardTitle className="font-semibold">Token URI</CardTitle>
+              <CardDescription>
+                Here you can see how the sponsorships have modified your
+                TBA&apos;s tokenURI
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="whitespace-prewrap overflow-hidden">
+                <code>{JSON.stringify(parsedTokenUri, null, 2)}</code>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      ) : (
+        "No image found!"
+      )}
     </main>
   );
 }
